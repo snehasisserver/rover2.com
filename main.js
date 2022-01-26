557ff35fc35a35f584f8f7254d746e066b42a3c3
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-01-26 14:59:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+rover_width = 100;
+rover_height = 90;
+
+rover_x = 10;
+rover_y = 10;
+
+var background_Img = "mars.jpg";
+var rover_Img = "rover.png";
+
+function add(){
+    var background_imgTag=new Image();
+    var rover_imgTag=new Image();
+
+    background_imgTag.onload=uploadBackground;
+    rover_imgTag.onload=uploadrover;
+
+    background_imgTag.src=background_Img;
+    rover_imgTag.src=rover_Img;
+
+    function uploadBackground(){
+        ctx.drawImage(background_imgTag, 0, 0,canvas.width, canvas.height);
+    }
+    function uploadrover(){
+        ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+    }   
+}
+
+
+
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if(keyPressed == '38'){
+         up();
+         console.log("up");
+    }
+    if(keyPressed == '40'){
+        down();
+        console.log("down");
+    }
+    if(keyPressed == '39'){
+        right();
+    console.log("right");
+    }
+    if(keyPressed == '37'){
+        left();
+    console.log("left");
+    }
+}
+
+function up(){
+    if(rover_y >=0){
+        rover_y = rover_y - 10 ;
+        console.log("When up arrow is clicked: X =" + rover_x + " , " + " Y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+function down(){
+    if(rover_y <=500){
+        rover_y = rover_y + 10 ;
+        console.log("When down arrow is clicked: X =" + rover_x + " , " + " Y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+function right(){
+    if(rover_x >=0){
+        rover_x = rover_x - 10 ;
+        console.log("When right arrow is clicked: X =" + rover_x + " , " + " Y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
+function left(){
+    if(rover_x <=700){
+        rover_x = rover_x + 10 ;
+        console.log("When left arrow is clicked: X =" + rover_x + " , " + " Y = " + rover_y);
+        uploadBackground();
+        uploadrover();
+    }
+}
